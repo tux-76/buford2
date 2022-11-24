7b1d868a376f70ac70ed0e0b9983c5c8cc0dd9ce
tux-76 <tux76@proton.me>
2022-11-24 11:27:00 -0600

    remove ids, fix main import

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ import { debug, toString } from "./module/private-alge.js"
 import * as algeFunc from "./module/public/alge.js";
 import * as factor from "./module/public/factor.js"
 
-let Buford2 = {alge:{}};
-Buford2.factor = factor;
+export let alge = {};
+export let factor = factor;
 
 function algeFunctionInputs(mode, string, ...otherArgs) {
 	if (mode === "simplifyExpression") 
@@ -39,7 +39,6 @@ for (const key in algeFunc) {
 	Buford2.alge[key] = (...args) => algebraFunctionWrap(key, ...args);
 }
 
-export default Buford2;
 console.log("Buford2 module exported.");
 
 /*
