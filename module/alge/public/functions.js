cab95a27309f8ad5f4bbe77dfe445da8c2b91b58
tux-76 <tux76@proton.me>
2023-03-15 19:28:20 +0000

    original reorganize - errors

---
+++ module/alge/public/functions.js
@@ -0,0 +1,5 @@
+import simplifyExpression from "./functions/simplifyExpression.js";
+import doubleSidedSolve from "./functions/doubleSidedSolve.js";
+import substituteVariable from "./functions/substituteVariable.js";
+
+export { simplifyExpression, doubleSidedSolve, substituteVariable };
