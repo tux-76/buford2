8f55841ba7e76bc54b345b305b175277e2adbb00
“tux-76” <tux76@proton.me>
2023-04-01 15:35:18 -0400

    create solve formula file

---
+++ module/alge/public/functions/solveFormula.js
@@ -0,0 +1,10 @@
+/*
+    SOLVE FORMULA
+
+    DESC: Will solve the formula inputted and return a version solved for the variable.
+
+    INPUTS: 
+        1: Fomula (as equation)
+        2: Formula key such as: {0: any}
+        3: Variable index to solve for (leave blank if obvious, AI can tell)
+*/
